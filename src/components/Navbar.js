aa0e21987babebeef1c02d382daa39119d00eaff
Fabian Gallegos Zapata <fzapata658@gmail.com>
2019-04-04 13:03:01 -0600

    primer commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react'
+import './node_modules/startbootstrap-creative/css/creative.min.css'
+
+
+
+class Navbar extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            
+        } 
+    }
+}  
